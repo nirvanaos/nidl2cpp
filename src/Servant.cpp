eda93a8a68dc8440b70a49857d26dea022eb2853
Igor Popov <popov.nirvana@gmail.com>
2023-09-20 12:25:31 +0300

    Servant includes restore normal

--- src/Servant.cpp
+++ src/Servant.cpp
@@ -37,17 +37,10 @@ void Servant::end (const Root&)
 void Servant::leaf (const Include& item)
 {
 	if (!options ().no_servant) {
-		h_ << "#include ";
-		
-		if (item.system ()) {
-			h_ << '<'
-				<< path (path (item.file ()).replace_extension ("").string () + options ().client_suffix)
-				.replace_extension ("h").string () << ">\n";
-		} else {
-			h_ << '"'
-				<< path (path (item.file ()).replace_extension ("").string () + options ().servant_suffix)
-				.replace_extension ("h").string () << "\"\n";
-		}
+		h_ << "#include " << (item.system () ? '<' : '"')
+			<< path (path (item.file ()).replace_extension ("").string () + options ().servant_suffix).replace_extension ("h").string ()
+			<< (item.system () ? '>' : '"')
+			<< std::endl;
 	}
 }
 
